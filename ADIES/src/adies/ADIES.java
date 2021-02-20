b1adf12aa6146eb2d3e0e2df748797b2979a1481
GJHR-6 <62277625+GJHR-6@users.noreply.github.com>
2021-02-19 19:18:50 -0600

    Inicio de programa
    Comentario de bienvenida

---
+++ ADIES/src/adies/ADIES.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adies;
+
+/**
+ *
+ * @author guill
+ */
+public class ADIES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Hola, bienvenido no hemos hecho nada del proyecto :D
+    }
+    
+}
